acd8cca23ab884aaef7c1c07f45e819be71f6a68
Katsuragi <89945675+CHristopherAkko@users.noreply.github.com>
2021-09-21 11:18:38 -0500

    Add files via upload

---
+++ hola_mundo.py
@@ -0,0 +1,23 @@
+#Variables de mensaje
+message1 = "Hola mundo"
+
+message2 = "Adios mundo"
+print (message1, message2)
+
+#Mayusculas y minusculas
+print (message2.upper())
+print (message2.lower())
+
+#Variable nombre y apellido
+nombre = "Chris"
+apellido = "Barajas"
+nombre_completo = nombre + " " + apellido
+print (nombre_completo)
+
+
+nombre_completo_f_string = f"{nombre} {apellido}"
+print (nombre_completo_f_string)
+
+texto = "uno\ndos\ttres"
+
+print (texto)
